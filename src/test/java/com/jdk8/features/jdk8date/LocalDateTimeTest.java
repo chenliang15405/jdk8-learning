a2c904553fc628ceaa5708b583e7a1dfb5aa24be
chenliang15405 <1165243776@qq.com>
2020-05-01 21:56:37 +0800

    feat：jdk8时间api

--- src/test/java/com/jdk8/features/jdk8date/LocalDateTimeTest.java
+++ src/test/java/com/jdk8/features/jdk8date/LocalDateTimeTest.java
@@ -4,7 +4,9 @@ import org.junit.Test;
 
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalUnit;
 
 /**
  *  LocalDate   LocalTime   LocalDateTime
@@ -13,7 +15,9 @@ import java.time.temporal.TemporalAdjusters;
  *
  *  Duration 两个“时间”之间的间隔
  *
- *  Period 两个“日期” 之间的间隔
+ *  Period 两个“日期” 之间的间隔(相差几年几月几天这样子，不能获取具体的相差多少天)
+ *
+ *  ChronoUnit.DAYS、ChronoUnit.MONTHS 等 可以直接获取日期之间相差的天数、月数等
  *
  *  TemporalAdjuster 时间校正器
  *      TemporalAdjusters  该类通过静态方法提供了大量的常用TemporalAdjuster的实现
@@ -40,7 +44,7 @@ public class LocalDateTimeTest {
     @Test
     public void test2(){
         Instant now = Instant.now();  // 默认获取的UTC时区
-        System.out.println(now);
+        System.out.println(now); // 获取时间
 
         OffsetDateTime offset = now.atOffset(ZoneOffset.ofHours(8));
         System.out.println(offset);
@@ -114,12 +118,70 @@ public class LocalDateTimeTest {
     }
 
     @Test
+    public void test9() {
+        DateTimeFormatter df = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
+
+        System.out.println(now.format(df));
+    }
+
+    /**
+     * Duration 获取两个时间间隔，只可以获取LocalDateTime 和Instant 这种带秒的
+     */
+    @Test
+    public void test10() {
+        LocalDateTime ld1 = LocalDateTime.now();
+        LocalDateTime ld2 = LocalDateTime.of(2020, 6, 1, 12, 30,30);
+
+        Duration between = Duration.between(ld1, ld2);
+        System.out.println(between.toDays());
+    }
+
+    /**
+     * Period 用法 获取两个时间间隔
+     */
+    @Test
+    public void test11() {
+        LocalDate ld1 = LocalDate.now();
+        LocalDate ld2 = LocalDate.of(2021, Month.JUNE, 1);
+        Period between = Period.between(ld2, ld1);
+
+        // 注意，这里Period获取的相差时间是已经换算成了指定的年月日了
+        // 就是说：现在距离2021-06-01 相差了 1年 1月 2 日，不可以直接获取相差多少天
+        System.out.printf("相差： %d年 %d月 %d天", Math.abs(between.getYears()), Math.abs(between.getMonths()), Math.abs(between.getDays()));
+
+        System.out.println();
+
+    }
+
+    @Test
+    public void test12() {
+        LocalDate ld1 = LocalDate.now();
+        LocalDate ld2 = LocalDate.of(2021, Month.JUNE, 1);
+        long betweenday = ChronoUnit.DAYS.between(ld1, ld2);
+        long betweenMonth = ChronoUnit.MONTHS.between(ld1, ld2);
+        long betweenSecond = ChronoUnit.SECONDS.between(ld1, ld2);
+
+        System.out.println("相隔秒数：" + betweenSecond);
+        System.out.println("相隔天数" + betweenday);
+        System.out.println("相隔月数" + betweenMonth);
+    }
+
+    @Test
     public void test7() {
         LocalDateTime now = LocalDateTime.now(ZoneId.of("Europe/Tallinn"));
         System.out.println(now);
 
+    }
 
-
+    /**
+     * 获取昨天日期
+     */
+    @Test
+    public void test8() {
+        String date = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                .withZone(ZoneId.systemDefault()).format(Instant.now().minus(1, ChronoUnit.DAYS));
+        System.out.println(date);
     }
 
 
